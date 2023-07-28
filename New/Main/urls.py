3fafa468c716490f51e483c5db64690fab3f233f
sushaantkhadka <sushantkhadka2060@gmail.com>
2023-07-28 07:49:31 +0545

    deleted the previous django project

--- New/Main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from Main import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-]
--- New/Main/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, Httpresponse
-
-# Create your views here.
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
